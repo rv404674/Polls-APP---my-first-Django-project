ba5a6b109a9b1c717e2e91628ad804d5520ac19c
Rahul Verma <rv404674@gmail.com>
2018-09-25 22:16:32 +0530

    Added Generic Views for all functions

--- sampsite/polls/views.py
+++ sampsite/polls/views.py
@@ -12,6 +12,15 @@ from django.http import HttpResponseRedirect
 
 from django.urls import reverse
 
+from django.views import generic
+
+from django.utils import timezone
+
+
+#now we will use generic view
+
+
+'''
 # display 5 latest question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -24,20 +33,45 @@ def index(request):
     }
 
     return render(request, 'polls/index.html', context)
+'''
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
+class DetailView(generic.DetailView):
+    model = Question
+
+    template_name = 'polls/detail.html'
+
+'''
 # this function display questions and choices assigned to it
 def detail(request, question_id):
 
     question = get_object_or_404(Question, pk=question_id)
     #return HttpResponse(f"you're looking at question {question_id}")
     return render(request, 'polls/detail.html', {'question':question})
+'''
 
+class ResultsView(generic.DetailView):
+    model = Question
+
+    template_name = 'polls/results.html'
+
+'''
 #this will display results
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     #response = f"You're looking at the results of question {question_id}"
     return render(request, 'polls/results.html', {'question': question})
+'''
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
